6f97f6b5f1f6bd3f6c695a4118d516ae4879453a
CedricRev <cedric.revestir@gmail.com>
2025-07-06 15:34:11 +0800

    Fixed errors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { loadDatabase } from "./scripts/sqlite";
+// import { loadDatabase } from "./scripts/sqlite";
 
 createApp(App).mount("#app");
 
-async function setup() {
-    await loadDatabase()
-}
+// async function setup() {
+//     await loadDatabase()
+// }
 
-window.addEventListener("DOMContentLoaded", () => {
-    setup()
-});
+// window.addEventListener("DOMContentLoaded", () => {
+//     setup()
+// });
