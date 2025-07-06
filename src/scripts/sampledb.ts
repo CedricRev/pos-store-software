1d09d8c1f9fad2790a6345675b24a2291c2eab17
CedricRev <cedric.revestir@gmail.com>
2025-07-06 14:46:25 +0800

    Make temp data

---
+++ src/scripts/sampledb.ts
@@ -0,0 +1,22 @@
+export const categories = [
+    {
+        id: "1",
+        displayname: "Utensils",
+        thumbnail: ""
+    },
+    {
+        id: "2",
+        displayname: "Wearables",
+        thumbnail: ""
+    },
+    {
+        id: "3",
+        displayname: "Tools",
+        thumbnail: ""
+    },
+    {
+        id: "4",
+        displayname: "Accessories",
+        thumbnail: ""
+    }
+]
--- src/scripts/sqlite.ts
+++ src/scripts/sqlite.ts
@@ -1,5 +1,5 @@
-import Database from '@tauri-apps/plugin-sql';
+// import Database from '@tauri-apps/plugin-sql';
 
-export async function loadDatabase() {
-    const db = await Database.load('sqlite:pos-store.db');
-}
+// export async function loadDatabase() {
+//     const db = await Database.load('sqlite:pos-store.db');
+// }
