ec85f3159fdf7a4af55548c134a73273e57a2e7b
CedricRev <cedric.revestir@gmail.com>
2025-07-18 11:07:09 +0800

    Add tauri-fs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,10 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_autostart::init(
             tauri_plugin_autostart::MacosLauncher::LaunchAgent,
-            None
+            None,
         ))
         .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
         .plugin(tauri_plugin_sql::Builder::new().build())
